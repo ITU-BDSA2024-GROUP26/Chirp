543af2469360b89a0dbc6cc15685d78c9f233028
Torbjørn Johannsen <toej@itu.dk>
2024-09-03 14:20:02 +0200

    Self made regex patterns

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -2,7 +2,13 @@
 using System.Text.RegularExpressions;
 
 const string csvPath = "chirp_cli_db.csv";
-// test comment 
+// recognises anything inbetween two quotation marks and arbitrary spaces, with a capture group excluding quotation marks 
+Regex patMsg = new Regex("(?:\\s*\"+\\s*)(.+)(?:\\s*\"+\\s*)");
+// Captures a continuous word with a ',' and spaces behind it 
+Regex patName = new Regex("(\\w+)(?:\\s*,\\s*)");
+// captures a number of arbitrary length with a ',' and spaces in front
+Regex patTime = new Regex("(?:\\s*,\\s*)(\\d+)");
+
 switch (args[0])
 {
     case "read":
@@ -12,12 +18,13 @@ switch (args[0])
         reader.ReadLine();
         while (reader.ReadLine() is { } line)
         {
-            var splitLine = csvParser.Split(line);
-            var user = splitLine[0];
-            var unixTime = long.Parse(splitLine[^1]);
+            if(!patName.IsMatch(line) || !patMsg.IsMatch(line) || !patTime.IsMatch(line)) { continue; }
+            //var splitLine = csvParser.Split(line);
+            var user = patName.Match(line).Groups[1].Value;
+            var unixTime = long.Parse(patTime.Match(line).Groups[1].Value);
             var dateTime = DateTimeOffset.FromUnixTimeSeconds(unixTime).ToLocalTime();
             var formattedTime = dateTime.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture);
-            var message = splitLine[1].Substring(1, splitLine[1].Length - 2);
+            var message = patMsg.Match(line).Groups[1].Value;
             Console.WriteLine($"{user} @ {formattedTime}: {message}");
         }
 
