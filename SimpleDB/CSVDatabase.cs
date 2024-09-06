c244bc7c8cefcde9c30198a8adadea899aac0906
Torbjørn Johannsen <toej@itu.dk>
2024-09-06 12:28:37 +0200

    Added CsvConfiguration for reading
    The creators of the csvhelper library seem to have intended that clients use the CsvConfiguration class to configure their readers.
    Thus we refactored our CsvDatabase to do this
    
    Co-authored-by: Gutti <gutt@itu.dk>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -11,19 +11,23 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
     private readonly string csvPath;
     private readonly CsvConfiguration csvWriterConfig;
+    private readonly CsvConfiguration csvReaderConfig;
     public CSVDatabase(string csvPath)
     {
         this.csvPath = csvPath;
+
+        // invariant culture ensures proper parsing of decimal numbers(with .) as well as timestamps
         this.csvWriterConfig = new CsvConfiguration(CultureInfo.InvariantCulture) {
             ShouldQuote = (args) => false, // Ensures that we don't end up with double qoutes around qouted text  
             HasHeaderRecord = false // ensures that we don't write additional headers inside the file
         };
+        csvReaderConfig = new CsvConfiguration(CultureInfo.InvariantCulture);
     }
 
     public IEnumerable<T> Read(int? limit = null)
     {
         using (var reader = new StreamReader(csvPath)) 
-        using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        using (CsvReader csv = new CsvReader(reader, csvReaderConfig))
         {
             if(limit == null) { return csv.GetRecords<T>().ToList<T>(); }
 
