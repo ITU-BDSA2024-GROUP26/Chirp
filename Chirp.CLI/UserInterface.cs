8ba746d45605b7e894a6a16e0669dabfe5dac942
Jovana <jnov@itu.dk>
2024-09-07 12:45:56 +0200

    print method is taking a collection instead of cheep

--- Chirp.CLI/UserInterface.cs
+++ Chirp.CLI/UserInterface.cs
@@ -8,10 +8,16 @@ namespace Chirp.CLI;
 
 public static class UserInterface
 {
-    public static void printCheeps(Cheep record) {
-
-            var dateTime = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp).ToLocalTime();
-            Console.WriteLine(record.Author +" @ " + dateTime.ToString("MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture) +  ": " + record.Message);
+    public static void PrintCheeps(IEnumerable<Cheep> cheeps) {
+       
+        foreach (var cheep in cheeps)
+        {
+            var dateTime = DateTimeOffset.FromUnixTimeSeconds(cheep.Timestamp).ToLocalTime();
+            Console.WriteLine(cheep.Author +" @ " + dateTime.ToString("MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture) +  ": " + cheep.Message);
+        }
+        
+            //var dateTime = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp).ToLocalTime();
+            //Console.WriteLine(record.Author +" @ " + dateTime.ToString("MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture) +  ": " + record.Message);
     
 
 }
