1e3745c5ea9251eef606ab6c8b8b37de78751fc3
Jovana <jnov@itu.dk>
2024-09-12 13:40:30 +0200

    working on tests

--- tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
+++ tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
@@ -3,8 +3,36 @@ namespace Chirp.CLI.SimpleDB.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void readTest()
     {
+    //arange 
+    const string tempCsv = "tempTestFile.csv";
+    IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+    Cheep cheep = new Cheep("bobo","Helooo", 1725366268);
+    
+    //act 
+    database.Store(cheep);
+    var record = database.Read(); 
 
+    //assert
+    foreach (var output in record)
+        {
+            Assert.Equal(output,cheep);
+        }
+    
+    }
+
+    [Fact]
+    public void storeTest() 
+    {
+    //arange 
+    const string tempCsv = "tempTestFile.csv";
+    IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+    
+
+    //act 
+
+
+    //assert    
     }
 }
