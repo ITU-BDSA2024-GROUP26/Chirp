10b08d535f08088d33e72be7af8fd21322426daf
Jovana <jnov@itu.dk>
2024-09-12 16:54:45 +0200

    Add passed IntegrationTests on database
    We are testing for storing and reading from the database. To assure optimal functionality
    
    Co-authored-by: Annam <abch@itu.dk>

--- tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
+++ tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
@@ -2,37 +2,44 @@ namespace Chirp.CLI.SimpleDB.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void readTest()
-    {
-    //arange 
-    const string tempCsv = "tempTestFile.csv";
-    IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
-    Cheep cheep = new Cheep("bobo","Helooo", 1725366268);
     
-    // //act 
-    // database.Store(cheep);
-    var record = database.Read(); 
-
-    //assert
-    foreach (var output in record)
-        {
-            Assert.Equal(output,cheep);
-        }
-    
-    }
-
     [Fact]
     public void storeTest() 
     {
-    //arange 
-    const string tempCsv = "tempTestFile.csv";
-    IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
-    
+        //arange 
+        const string tempCsv = "/Users/jovananovovic/Documents/Chirp/tests/Chirp.CLI.CSVDatabase.Tests/tempTestFile.csv";
+        IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+        Cheep cheep = new Cheep("juju","Hello kitti <3 ;)", 1690979858);
 
-    //act 
+        //act
+        database.Store(cheep);
+        var record = database.Read();
 
-
-    //assert    
+        //assert  
+        foreach (var output in record)
+        {
+            Assert.Equal(cheep,output);
+        }
     }
+    
+    [Fact]
+    public void readTest()
+    {
+        
+        //arange 
+        const string tempCsv = "/Users/jovananovovic/Documents/Chirp/tests/Chirp.CLI.CSVDatabase.Tests/tempTestFile.csv";
+        IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+        Cheep cheep = new Cheep("juju","Hello kitti <3 ;)", 1690979858);
+        
+        // //act 
+        var record = database.Read(); 
+
+        //assert
+        foreach (var output in record)
+        {
+            Assert.Equal(cheep,output);
+        }
+    
+    }
+    
 }
