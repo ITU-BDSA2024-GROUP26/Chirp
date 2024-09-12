ebe772d1e1e3da9bd4142d8118270641ff16f8a7
Gutti <gutt@itu.dk>
2024-09-12 15:41:45 +0200

    Add project references in CSVDatabase test project
    Fix the .sln file
    rename some .csproj files to correspond with their project name

--- tests/Chirp.CLI.CSVDatabase.Tests/UnitTests.cs
+++ tests/Chirp.CLI.CSVDatabase.Tests/UnitTests.cs
@@ -1,10 +1,1 @@
 namespace Chirp.CLI.SimpleDB.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-
-    }
-}
--- tests/Chirp.CLI.Client.Tests/End2EndTests.cs
+++ tests/Chirp.CLI.Client.Tests/End2EndTests.cs
@@ -1,10 +1,1 @@
 namespace Chirp.CLI.Client.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-
-    }
-}
