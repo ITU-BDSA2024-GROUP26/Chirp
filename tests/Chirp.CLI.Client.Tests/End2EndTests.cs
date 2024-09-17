56209de0cecb43c317bfd94026ccb007dcebcb55
Vickihv <89073336+Vickihv@users.noreply.github.com>
2024-09-17 14:23:39 +0200

    Update End2EndTests.cs
    Add End To End Test

--- tests/Chirp.CLI.Client.Tests/End2EndTests.cs
+++ tests/Chirp.CLI.Client.Tests/End2EndTests.cs
@@ -1,10 +1,42 @@
+using System;
+using System.Diagnostics;
+using System.Globalization;
+using Xunit;
+using Chirp.CLI;
+
 namespace Chirp.CLI.Client.Tests;
 
-public class UnitTest1
+
+public class EndToEndTests
 {
     [Fact]
-    public void Test1()
-    {
 
+    public static void TestReadCheep()
+    {
+        // Arrange
+        const string csvPath = "chirp_cli_db3.csv";
+
+        // Act
+        string output = "";
+        using (var process = new Process())
+        {
+            process.StartInfo.FileName = "/opt/homebrew/bin/dotnet";
+            process.StartInfo.Arguments = "run read";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.WorkingDirectory = "/Users/vicki/Documents/Chirp/src/Chirp.CLI.Client";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            // Synchronously read the standard ouput of he spawned process.
+            StreamReader reader = process.StandardOutput;
+            output = reader.ReadToEnd();
+            process.WaitForExit();
+        }
+
+        string fstCheep = output.Split('\n')[0];
+
+        // Assert
+        Assert.StartsWith("ropf", fstCheep);
+        Assert.EndsWith("Hello, BDSA students!", fstCheep);
     }
 }
