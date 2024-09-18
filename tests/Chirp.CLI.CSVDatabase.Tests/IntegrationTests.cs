868ff33e2681bbe380b306fa62a419accd37fa57
Gutti <gutt@itu.dk>
2024-09-18 15:59:38 +0200

    Add singleton
    Closes #11
    
    Co-authored-by: Jovana jnov@itu.dk

--- tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
+++ tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
@@ -8,14 +8,14 @@ public class UnitTest1
     {
         //arange 
         const string tempCsv = "/Users/jovananovovic/Documents/Chirp/tests/Chirp.CLI.CSVDatabase.Tests/tempTestFile.csv";
-        IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+        CSVDatabase<Cheep>.SetPath(tempCsv);
         Cheep cheep = new Cheep("juju","Hello kitti <3 ;)", 1690979858);
 
         //act
-        database.Store(cheep);
-        var record = database.Read();
+        CSVDatabase<Cheep>.getInstance().Store(cheep);
+        var record = CSVDatabase<Cheep>.getInstance().Read();
 
-        //assert  
+        //assert
         foreach (var output in record)
         {
             Assert.Equal(cheep,output);
@@ -28,11 +28,11 @@ public class UnitTest1
         
         //arange 
         const string tempCsv = "/Users/jovananovovic/Documents/Chirp/tests/Chirp.CLI.CSVDatabase.Tests/tempTestFile.csv";
-        IDatabaseRepository<Cheep> database = new CSVDatabase<Cheep>(tempCsv);
+        CSVDatabase<Cheep>.SetPath(tempCsv);
         Cheep cheep = new Cheep("juju","Hello kitti <3 ;)", 1690979858);
         
         // //act 
-        var record = database.Read(); 
+        var record = CSVDatabase<Cheep>.getInstance().Read();
 
         //assert
         foreach (var output in record)
