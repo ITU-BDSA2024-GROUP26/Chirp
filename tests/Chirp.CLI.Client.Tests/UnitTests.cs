f5d7e5636a73df455432ba055e086c9545a74919
torbjornjohannsen <toej@itu.dk>
2024-09-23 12:53:39 +0200

    FormatTimeStampTest uses universal time

--- tests/Chirp.CLI.Client.Tests/UnitTests.cs
+++ tests/Chirp.CLI.Client.Tests/UnitTests.cs
@@ -5,12 +5,12 @@ namespace Chirp.CLI.Client.Tests;
 public class UnitTests
 {
     [Theory]
-    [InlineData(0, "01/01/70 01:00:00")]
-    [InlineData(1, "01/01/70 01:00:01")]
-    [InlineData(1690891760, "08/01/23 14:09:20")]
+    [InlineData(0, "01/01/70 00:00:00")]
+    [InlineData(1, "01/01/70 00:00:01")]
+    [InlineData(1690891760, "08/01/23 12:09:20")]
     public void FormatTimestampTest(long unixTime, string expectedFormattedTime)
     {
-        Assert.Equal(expectedFormattedTime, DateTimeOffset.FromUnixTimeSeconds(unixTime).ToLocalTime()
+        Assert.Equal(expectedFormattedTime, DateTimeOffset.FromUnixTimeSeconds(unixTime).ToUniversalTime()
             .ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture));
     }
 }
