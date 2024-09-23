ce8e7a847e1cfc714d2fd1fcf3381aaa0fd3623c
torbjornjohannsen <toej@itu.dk>
2024-09-23 12:38:15 +0200

    Added comments

--- tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
+++ tests/Chirp.CLI.CSVDatabase.Tests/IntegrationTests.cs
@@ -7,14 +7,18 @@ public class UnitTest1
     [Fact]
     public void StoreTest() 
     {
-        //arange 
+        //ARRANGE 
+        
+        // Ensure that we have a csv file that won't contain anything other than what we want it to
         const string tempCsv = "tempTestFile.csv";
         FileInfo fInfo = new(tempCsv);
         if(fInfo.Exists) {
             fInfo.Delete();
         }
+
         // we are NOT testing whether the file is created in the proper manner at the moment
         // so we ensure that the headers are correct
+        // (we should have another unit test to test that functionality)
         using (var writer = fInfo.AppendText()) 
         {
             writer.WriteLine("Author,Message,Timestamp");
@@ -22,7 +26,8 @@ public class UnitTest1
         }
 
 
-        CSVDatabase<Cheep>.SetPath(fInfo.FullName);
+        // create our test data
+        CSVDatabase<Cheep>.SetPath(tempCsv);
         Cheep cheep = new Cheep("juju2","Hello kitti <3 ;)", 1690979858);
 
         //act
@@ -40,6 +45,9 @@ public class UnitTest1
     [InlineData("ropf", "Hello, BDSA students!", 1690891760)]
     public void readTest(string author, string message, int timestamp)
     {
+        // ARRANGE
+
+        // first some random test data to ensure that we don't get any fuckery from an empty csv file
         string[] testAuthors = { "adho", "adho", "ropf"};
         string[] testMessages = {
             "\"Welcome to the course!\"", 
@@ -47,17 +55,19 @@ public class UnitTest1
             "\"Cheeping cheeps on Chirp :)\""
         };
         int[] timestamps = { 1690978778,  1690979858,  1690981487 };
-        //arange 
-        //arange 
+
+        // Then ensure that we create a new file that ONLY contains what we want it to
         const string tempCsv = "tempTestFile.csv";
         FileInfo fInfo = new(tempCsv);
         if(fInfo.Exists) {
             fInfo.Delete();
         }
+
+        // then we create our cheeps and write them to the file
+        // they will be indentical UNLESS csvhelper fails, which we assume it doesn't
         List<Cheep> cheeps = new List<Cheep>(); 
-        // we are NOT testing whether the file is created in the proper manner at the moment
-        // so we ensure that the headers are correct
         using var writer = new StreamWriter(fInfo.FullName, true);
+        // note the csvconfig was stolen from the CSVDatabase code
         using var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture) {
             ShouldQuote = (args) => false, // Ensures that we don't end up with double qoutes around qouted text  
             HasHeaderRecord = false // ensures that we don't write additional headers inside the file
@@ -70,10 +80,14 @@ public class UnitTest1
         csvWriter.WriteRecord(cheeps.Last());
         csvWriter.NextRecord();
 
-        CSVDatabase<Cheep>.SetPath(tempCsv);
         //act 
+        // finally we let CSVDatabase retrieve the files
+        // note that the database should be able to find the file in the local directory
+        // thus we give it the relative path
+        CSVDatabase<Cheep>.SetPath(tempCsv);
         var records = CSVDatabase<Cheep>.getInstance().Read();
         //assert
+        // and finally we simply test if we get back out what we put in 
         int k = 0;
         foreach (var output in records)
         {
