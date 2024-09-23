1ff2b69a68451ce2873b8fce2032dbe7b8f9dbbf
torbjornjohannsen <toej@itu.dk>
2024-09-23 11:34:02 +0200

    can now request x amount of cheeps

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -2,13 +2,15 @@ using SimpleDB;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-//CSVDatabase<Cheep>.SetPath("csvdb.csv");
+
 List<Cheep> cheepList = new List<Cheep>(100);
 
+app.MapGet("/cheeps/{num}", (int num) => cheepList.GetRange(0, num));
 app.MapGet("/cheeps", () => cheepList);
 app.MapPost("/cheep", (Cheep cheep) => {
     cheepList.Add(cheep);
     });
 app.Run();
+
 
 public record Cheep(string Author, string Message, long Timestamp);
