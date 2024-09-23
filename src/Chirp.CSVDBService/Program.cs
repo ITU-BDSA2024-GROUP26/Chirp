b7acdd01072238912292a07cb914813df4ac5ebb
torbjornjohannsen <toej@itu.dk>
2024-09-23 11:43:12 +0200

    DB Service handles overflow of requested # values

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -5,7 +5,12 @@ var app = builder.Build();
 
 List<Cheep> cheepList = new List<Cheep>(100);
 
-app.MapGet("/cheeps/{num}", (int num) => cheepList.GetRange(0, num));
+app.MapGet("/cheeps/{num}", (int num) => {
+    if(num >= cheepList.Count) {
+        return cheepList;
+    }
+    return cheepList.GetRange(0, num);
+    });
 app.MapGet("/cheeps", () => cheepList);
 app.MapPost("/cheep", (Cheep cheep) => {
     cheepList.Add(cheep);
