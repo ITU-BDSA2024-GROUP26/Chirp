815e56738b625e87c4b7f691ee4f4a9140a38942
torbjornjohannsen <toej@itu.dk>
2024-09-25 12:53:50 +0200

    new db file cause fuckery

--- post_initial_data.py
+++ post_initial_data.py
@@ -5,7 +5,8 @@ names = ['ropf', 'adho', 'adho', 'ropf']
 messages = ['\"Hello, BDSA students!\"', '\"Welcome to the course!\"', '\"I hope you had a good summer.\"', '\"Cheeping cheeps on Chirp :)\"']
 times = [1690891760, 1690978778, 1690979858, 1690981487]
 
-#resp = requests.post('https://bdsagroup26chirpremotedb.azurewebsites.net/cheep', json={"author": "testAuthor", "message": "testMessage", "timestamp": int(time.time())})
+# https://bdsagroup26chirpremotedb.azurewebsites.net
+# http://localhost:5000
 
 for i in range(4):
     resp = requests.post('https://bdsagroup26chirpremotedb.azurewebsites.net/cheep', json={"Author": names[i], "Message": messages[i], "Timestamp": times[i]})
