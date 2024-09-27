8d2b6376f43546219cb59caf49c846b7990a6538
Gutti <gutt@itu.dk>
2024-09-27 13:36:21 +0200

    Refactor ObjectQuery method and started testing the database

--- src/Chirp.CSVDBService/Program.cs
+++ src/Chirp.CSVDBService/Program.cs
@@ -1,7 +1,8 @@
 using SimpleDB;
 using System.CommandLine;
+using Chirp.SQLite;
 
-    var rootCommand = new RootCommand();
+var rootCommand = new RootCommand();
     var csvPathOption = new Option<string>(
     aliases: new[] { "-p", "--path" },
     description: "Path to the CSV file",
@@ -11,6 +12,13 @@ rootCommand.AddGlobalOption(csvPathOption);
 
 rootCommand.SetHandler(async (string path) =>
 {
+    DBFacade.SetDBPath("db.db");
+    ISQLDatabase database2 = DBFacade.Instance;
+    foreach (var user in database2.ObjectQuery<User> ("SELECT * FROM user"))
+    {
+        Console.WriteLine(user);
+    }
+    
     CSVDatabase<Cheep>.SetPath(path);
     var builder = WebApplication.CreateBuilder(args);
     var app = builder.Build();
@@ -26,3 +34,8 @@ csvPathOption);
 await rootCommand.InvokeAsync(args);
 
 public record Cheep(string Author, string Message, long Timestamp);
+
+public record User(int user_id, string username, string email)
+{
+    public User() : this(-1, "defaultUserName", "defaultEmail") { }
+}
