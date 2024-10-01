bccd236196dfe0b47ef171039bef728f8c98925d
Gutti <gutt@itu.dk>
2024-10-01 11:15:24 +0200

    Make Razor page work correctly
    - Add a temp database
    - CheepService creates neccecary relations if they don't exist
    - GetCheepsFromAuthor now works correctly

--- src/Chirp.Razor/schema.sql
+++ src/Chirp.Razor/schema.sql
@@ -1,12 +1,13 @@
-﻿drop table if exists user;
-create table user (
+﻿-- Helge's .sql schema file but instead of dropping tables if they exist, it creates them if they don't exist.
+-- It is done this way so that the relations are created the first time the application is run and persist between runs.
+
+create table if not exists user (
   user_id integer primary key autoincrement,
   username string not null,
   email string not null
 );
 
-drop table if exists message;
-create table message (
+create table if not exists message (
   message_id integer primary key autoincrement,
   author_id integer not null,
   text string not null,
