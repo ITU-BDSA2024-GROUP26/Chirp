5196cf4bc15bf475a9fb11011926b420a943f708
Jovana <jnov@itu.dk>
2024-10-02 22:06:17 +0200

    Give EF Core context on db schema

--- src/Chirp.Razor/CheepDBContext.cs
+++ src/Chirp.Razor/CheepDBContext.cs
@@ -3,9 +3,11 @@ using Microsoft.Extensions.Options;
 
 namespace Chirp.Razor;
 
-public class CheepDBContext
+public class CheepDBContext : DbContext
 {
     DbSet<Cheep> Cheeps { get; set; }
     DbSet<Author> Authors { get; set; }
+
+    public CheepDBContext(DbContextOptions<CheepDBContext>options): base(options) {}
     
 }
