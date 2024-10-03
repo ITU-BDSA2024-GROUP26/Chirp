86b582e3ae0e0627e14c75902456330aa346f098
torbjornjohannsen <toej@itu.dk>
2024-10-03 12:49:20 +0200

    Add DTO classes
    Co-authored-by: Gutti gutt@itu.dk
    Co-authored-by: Annam abch@itu.dk
    Co-authored-by: Jovana jnov@itu.dk
    Co-authored-by: Vicki vbjo@itu.dk

--- src/Chirp.Razor/Cheep.cs
+++ src/Chirp.Razor/Cheep.cs
@@ -7,5 +7,5 @@ public class Cheep
     public int CheepId { get; set; }
     public Author Author { get; set; }
     public string Text{ get; set; } 
-    public DateTime Timestamp{ get; set; } 
+    public DateTime TimeStamp{ get; set; } 
 }
