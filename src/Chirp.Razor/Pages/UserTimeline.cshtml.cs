4e3a52cd43e737f157ee7570ec8686f4d290df15
torbjornjohannsen <toej@itu.dk>
2024-10-03 16:17:03 +0200

    Fix repo with razor pages
    Co-authored-by: Gutti gutt@itu.dk
    Co-authored-by: Annam abch@itu.dk
    Co-authored-by: Jovana jnov@itu.dk

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -6,7 +6,7 @@ namespace Chirp.Razor.Pages;
 public class UserTimelineModel : PageModel
 {
     private readonly IChirpRepository _service;
-    public IEnumerable<CheepDTO> Cheeps { get; set; }
+    public ICollection<CheepDTO> Cheeps { get; set; }
 
     public UserTimelineModel(IChirpRepository service)
     {
