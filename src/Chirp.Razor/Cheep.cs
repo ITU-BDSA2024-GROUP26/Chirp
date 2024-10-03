2a8a89f0b42a9f501576f2a8e910e16135cc8e40
torbjornjohannsen <toej@itu.dk>
2024-10-03 15:30:07 +0200

    Database initialization
    Co-authored-by: Gutti gutt@itu.dk
    Co-authored-by: Annam abch@itu.dk
    Co-authored-by: Jovana jnov@itu.dk

--- src/Chirp.Razor/Cheep.cs
+++ src/Chirp.Razor/Cheep.cs
@@ -6,6 +6,7 @@ public class Cheep
 {
     public int CheepId { get; set; }
     public Author Author { get; set; }
+    public int AuthorId { get; set; } 
     public string Text { get; set; } 
     public DateTime TimeStamp { get; set; } 
 }
