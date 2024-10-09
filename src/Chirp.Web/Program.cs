3c2a6107e27547eb0fb6d5b24e94d262ee484335
torbjornjohannsen <toej@itu.dk>
2024-10-09 15:57:14 +0200

    Use production database connection string if enviroment is production

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -11,15 +11,23 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddRazorPages();
-        
+
         // Load database connection via configuration
-        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        // where we differentiate between production and development/staging/anything else 
+        // Note that enviroment like this is set via the ASPNETCORE_ENVIRONMENT enviroment variable 
+        // this is set globally to Production on our Azure server, so we don't need to worry about anything
+        string? connectionString; 
+        if(builder.Environment.IsProduction()) {
+            connectionString = builder.Configuration.GetConnectionString("ProductionConnection");
+        } else {
+            connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        }
         builder.Services.AddDbContext<CheepDBContext>(options => options.UseSqlite(connectionString));
 
         // add services via DI  
         builder.Services.AddScoped<ICheepRepository, CheepRepository>(); 
         builder.Services.AddScoped<ICheepService, CheepService>();
-
+        
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
