344277511033fecb776a308d874046aff806f380
Jovana <jnov@itu.dk>
2024-10-14 14:54:14 +0200

    fixing warnings

--- src/Chirp.Core/Chirp.Core.Entities/Author.cs
+++ src/Chirp.Core/Chirp.Core.Entities/Author.cs
@@ -1,8 +1,11 @@
 namespace Chirp.Core.Entities;
+using System.ComponentModel.DataAnnotations;
 
 public class Author
 {
     public int AuthorId { get; set; }
+    
+    [Required] 
     public string Name { get; set; }
     public string Email { get; set; }
     public ICollection<Cheep> Cheeps { get; set; }
---
+++ src/Chirp.Infrastructure/Chirp.Infrastructure.Repositories/Migrations/20241013201803_fields.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class fields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Cheeps",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Text",
+                table: "Cheeps",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
