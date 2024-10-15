2291fa242b32500d34782f526a5f18981a358509
JojoStarman <151638823+JojoStarman@users.noreply.github.com>
2024-10-15 04:38:22 +0200

    add FindAuthorbyEmail, not tested

--- src/Chirp.Infrastructure/Chirp.Infrastructure.Repositories/ICheepRepository.cs
+++ src/Chirp.Infrastructure/Chirp.Infrastructure.Repositories/ICheepRepository.cs
@@ -8,6 +8,7 @@ public interface ICheepRepository
 {
     public Task CreateCheep(Cheep newCheep); 
     public Task<Author?>FindAuthorbyName(string name); 
+    public Task<Author?>FindAuthorbyEmail(string email); 
     public Task<ICollection<Cheep>> ReadCheeps(int limit, int offset, string? authorNameRegex); 
 
     // given a cheep ID this method updates the 
