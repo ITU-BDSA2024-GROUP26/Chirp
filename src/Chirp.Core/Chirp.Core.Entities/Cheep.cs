be75380e873af5e3fdff6f3381ab05f42629108e
Vickihv <89073336+Vickihv@users.noreply.github.com>
2024-10-21 22:38:53 +0200

    Fix warnings in Cheep.cs

--- src/Chirp.Core/Chirp.Core.Entities/Cheep.cs
+++ src/Chirp.Core/Chirp.Core.Entities/Cheep.cs
@@ -3,8 +3,8 @@ namespace Chirp.Core.Entities;
 public class Cheep
 {
     public int CheepId { get; set; }
-    public Author Author { get; set; }
-    public int AuthorId { get; set; } 
-    public string Text { get; set; } 
-    public DateTime TimeStamp { get; set; } 
+    public Author? Author { get; set; }
+    public int AuthorId { get; set; }
+    public string? Text { get; set; }
+    public DateTime TimeStamp { get; set; }
 }
