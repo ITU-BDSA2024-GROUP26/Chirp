ef12bd8d6cb9121e25e9682628d710c41a07538f
Vickihv <89073336+Vickihv@users.noreply.github.com>
2024-10-21 22:38:39 +0200

    Fix warnings in Author.cs

--- src/Chirp.Core/Chirp.Core.Entities/Author.cs
+++ src/Chirp.Core/Chirp.Core.Entities/Author.cs
@@ -3,9 +3,9 @@ namespace Chirp.Core.Entities;
 public class Author
 {
     public int AuthorId { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public ICollection<Cheep> Cheeps { get; set; }
-    
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public ICollection<Cheep>? Cheeps { get; set; }
+
 
 }
