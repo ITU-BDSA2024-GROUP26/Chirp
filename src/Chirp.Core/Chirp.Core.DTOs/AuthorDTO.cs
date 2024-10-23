47dc0e62d3259f7f29118f74430f558e7e7bfa1f
Gutti <gutt@itu.dk>
2024-10-23 15:19:17 +0200

    Comment out integration tests and add Required fields to Entities

--- src/Chirp.Core/Chirp.Core.DTOs/AuthorDTO.cs
+++ src/Chirp.Core/Chirp.Core.DTOs/AuthorDTO.cs
@@ -7,8 +7,5 @@ public class AuthorDTO(string name, string email)
     public string Name { get; set; } = name;
     public string Email { get; set; } = email;
 
-    public AuthorDTO(Author author) : this(
-        author.Name ?? throw new ArgumentNullException(nameof(author.Name), "Author cannot be null"),
-        author.Email ?? throw new ArgumentNullException(nameof(author.Email), "Email cannot be null"))
-    { }
+    public AuthorDTO(Author author) : this(author.Name, author.Email) { }
 }
