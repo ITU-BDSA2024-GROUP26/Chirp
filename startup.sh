679036c39fc50e2072c5b5d35bb87ae7f4c00a6c
torbjornjohannsen <toej@itu.dk>
2024-10-30 12:39:23 +0100

    fix data source string in startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 echo Execute any possible migrations:
-./bundle --connection {$DATABASE_PATH}
+./bundle --connection "Data Source=$DATABASE_PATH"
 
 echo Starting App 
 dotnet Chirp.Web.dll
