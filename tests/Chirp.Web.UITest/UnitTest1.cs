a2debfe962f17c8ad7605139d54790d3c360e4b2
torbjornjohannsen <toej@itu.dk>
2024-11-04 23:50:44 +0100

    Working UI tests via playwright

--- tests/Chirp.Web.UITest/UnitTest1.cs
+++ tests/Chirp.Web.UITest/UnitTest1.cs
@@ -1,7 +1,11 @@
 using System.Text.RegularExpressions;
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Chirp.Web;
 using NUnit;
+using NUnit.Framework.Internal;
+using System.Diagnostics;
 
 namespace Chirp.Web.UITest;
 
@@ -9,10 +13,22 @@ namespace Chirp.Web.UITest;
 [TestFixture]
 public class Tests : PageTest
 {
-    [SetUp]
-    public void Setup()
+    private Process server;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
     {
+        System.Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+        server = Process.Start("dotnet", "Chirp.Web.dll");
     }
+
+    [OneTimeTearDown]
+    public void Cleanup() 
+    {
+        server.Kill();
+        server.Dispose();
+    }
+
 
     [Test]
     public void Test1()
@@ -24,14 +40,17 @@ public class Tests : PageTest
     public async Task TestAuthorTimeline()
     {
         // arrange
-        await Page.GotoAsync("http://localhost:5273/");
+        Console.WriteLine("Starting test author timeline");
+        //Thread.Sleep(3000);
+        await Page.GotoAsync("http://localhost:5000");
 
         // act
-        await Page.Locator("p").Filter(new() { HasText = "Jacqualine Gilcoine They were" }).GetByRole(AriaRole.Link).ClickAsync();
+        await Page.Locator("p").Filter(new() { HasText = "Roger Histand You are here" }).GetByRole(AriaRole.Link).ClickAsync();
 
         // assert 
-        await Expect(Page.Locator("h2")).ToContainTextAsync("Jacqualine Gilcoine's Timeline");
-        await Expect(Page.Locator("#messagelist")).ToContainTextAsync("Jacqualine Gilcoine His initials were L. L. How do you think this steak is rather reserved, and your Krusenstern. — 08/01/23 13:15:54");
-    }
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Roger Histand's Timeline" })).ToBeVisibleAsync();
+        }
+
+
 
 }
