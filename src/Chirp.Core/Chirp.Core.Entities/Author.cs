e00622e98156c7ef897a2d5c09124f7c62550c1b
Gutti Torvadal <gutt@itu.dk>
2024-11-06 00:58:55 +0100

    Modified tests so they work again

--- src/Chirp.Core/Chirp.Core.Entities/Author.cs
+++ src/Chirp.Core/Chirp.Core.Entities/Author.cs
@@ -6,8 +6,8 @@ namespace Chirp.Core.Entities;
 public class Author : IdentityUser
 {
     [Required]
-    public required string Name { get; set; }
+    public string? Name { get; set; }
     
     [Required]
-    public required ICollection<Cheep> Cheeps { get; set; }
+    public ICollection<Cheep>? Cheeps { get; set; }
 }
