37cfb9497b0003855a2e769b1aad4bb6e8db0d93
Jovana <jnov@itu.dk>
2024-11-11 23:39:15 +0100

    add Cors

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -36,7 +36,14 @@ public class Program
         
         // Once you are sure everything works, you might want to increase this value to up to 1 or 2 years
         builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromHours(10));
-
+        
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowChirp", policy =>
+            {
+                policy.WithOrigins("https://bdsagroup26chirprazor.azurewebsites.net"); //Only allow chirp
+            });
+        });
         
         var app = builder.Build();
         
@@ -59,6 +66,9 @@ public class Program
         app.UseStaticFiles();
 
         app.UseRouting();
+        
+        // Enable CORS policy
+        app.UseCors("AllowChirp");
         
         app.UseCookiePolicy(new CookiePolicyOptions
         {
