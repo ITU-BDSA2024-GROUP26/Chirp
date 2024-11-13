47335ebeafc4ef1ffd08fae17b84303d2281b615
torbjornjohannsen <toej@itu.dk>
2024-11-13 16:12:28 +0100

    Changed to strict matching of username

--- src/Chirp.Infrastructure/Chirp.Infrastructure.Services/CheepService.cs
+++ src/Chirp.Infrastructure/Chirp.Infrastructure.Services/CheepService.cs
@@ -9,7 +9,7 @@ public class CheepService(ICheepRepository cheepRepository) : ICheepService
 {
     private const int MaxCheeps = 32;
 
-    public async Task<IEnumerable<CheepDTO>> GetCheepsAsync(int page, string authorRegex=".*")
+    public async Task<IEnumerable<CheepDTO>> GetCheepsAsync(int page, string? authorRegex=null)
     {
         var cheeps = await cheepRepository.ReadCheeps(MaxCheeps, (page - 1) * MaxCheeps, authorRegex);
         return cheeps.Select(cheep => new CheepDTO(cheep));
