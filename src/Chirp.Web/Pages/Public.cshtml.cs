a685dad290dcc62ac0f7db74bb758d83110615cd
Jovana <jnov@itu.dk>
2024-11-19 13:47:11 +0100

    improved CheepBox.cshtml.cs

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -9,45 +9,18 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Chirp.Razor.Pages;
 
-public class PublicModel(ICheepService service, ICheepRepository cheepRepository, UserManager<Author> userManager) : PageModel
+public class PublicModel(ICheepService service, ICheepRepository cheepRepository, UserManager<Author> userManager) 
+    : CheepBoxModel(cheepRepository, userManager)
 {
     public Author? Author { get; set; }
-    
-    [BindProperty]
-    [StringLength(160, ErrorMessage = "Maximum length is 160")]
-    public required string Message { get; set; }
+    public ICheepRepository CheepRepository = cheepRepository;
+
     public required IEnumerable<CheepDTO> Cheeps { get; set; }
-    
 
     public async Task<ActionResult> OnGetAsync([FromQuery] int page = 1)
     {
         Author = await userManager.GetUserAsync(User);
         Cheeps = await service.GetCheepsAsync(page);
         return Page();
-    }
-    
-    public async Task<ActionResult> OnPostAsync() {
-        if (!ModelState.IsValid)
-        {
-            ModelState.AddModelError(string.Empty, "ops");
-            return Page(); 
-        }
-
-        if (User.Identity != null && !User.Identity.IsAuthenticated)
-        {
-            return RedirectToPage("");
-        }
-        
-        // Create the new Cheep
-        var newCheep = new Cheep
-        {
-            Author = await userManager.GetUserAsync(User),                    // Set the Author
-            Text = Message,                     // Set the text from the form input (Message property)
-            TimeStamp = DateTime.UtcNow         // Set the current timestamp
-        };
-
-        // Save the new Cheep (assuming a SaveCheepAsync method exists in your service or repository)
-        await cheepRepository.CreateCheep(newCheep);
-        return RedirectToPage("");
     }
 }
