dcf0c546f58ede245388caac1b172f381775aa19
torbjornjohannsen <toej@itu.dk>
2024-11-19 13:25:08 +0100

    Following/Unfollowing works

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using Chirp.Core.DTOs;
+using Chirp.Core.Entities;
+using Chirp.Infrastructure.Repositories;
 using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel(ICheepService service) : PageModel
+public class UserTimelineModel(ICheepRepository cheepRepository, UserManager<Author> userManager, ICheepService service) : FollowModel(cheepRepository, userManager)
 {
     public required IEnumerable<CheepDTO> Cheeps { get; set; }
 
