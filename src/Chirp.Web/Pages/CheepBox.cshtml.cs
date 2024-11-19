b2ebb6d1fed07384e14f88025f0a2303706e6e38
Jovana <jnov@itu.dk>
2024-11-19 13:47:11 +0100

    improved CheepBox.cshtml.cs

---
+++ src/Chirp.Web/Pages/CheepBox.cshtml.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Chirp.Core.DTOs;
+using Chirp.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Chirp.Core.Entities;
+using Chirp.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+
+namespace Chirp.Razor.Pages;
+
+public class CheepBoxModel(ICheepRepository cheepRepository, UserManager<Author> userManager) : PageModel
+{
+    public Author? Author { get; set; }
+    
+    [BindProperty]
+    [StringLength(160, ErrorMessage = "Maximum length is 160")]
+    public required string Message { get; set; }
+    
+    public async Task<ActionResult> OnPostShareAsync() {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError(string.Empty, "ops");
+            return Page(); 
+        }
+
+        if (User.Identity != null && !User.Identity.IsAuthenticated)
+        {
+            return RedirectToPage("");
+        }
+        
+        // Create the new Cheep
+        var newCheep = new Cheep
+        {
+            Author = await userManager.GetUserAsync(User),                    // Set the Author
+            Text = Message,                     // Set the text from the form input (Message property)
+            TimeStamp = DateTime.UtcNow         // Set the current timestamp
+        };
+
+        // Save the new Cheep (assuming a SaveCheepAsync method exists in your service or repository)
+        await cheepRepository.CreateCheep(newCheep);
+        return RedirectToPage("");
+    }
+}
