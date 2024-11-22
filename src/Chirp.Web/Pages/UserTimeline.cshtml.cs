ac08a8cca6b4bc4ee0a97dd6933be327917c9478
torbjornjohannsen <toej@itu.dk>
2024-11-22 22:48:13 +0100

    Refactor timelines to inherit from TimelineModel

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -8,25 +8,11 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel(ICheepRepository cheepRepository, UserManager<Author> userManager, ICheepService service) : PageModel
+public class UserTimelineModel(ICheepRepository cheepRepository, UserManager<Author> userManager, ICheepService service) : TimelineModel(service, cheepRepository, userManager)
 {
-    public required IEnumerable<CheepDTO> Cheeps { get; set; }
-
-    private FollowModel _followModel; 
-    private FollowModel lazyGetFollowModel() 
-    {
-        if(_followModel == null) { _followModel = new FollowModel(cheepRepository, userManager, User); }
-        return _followModel;
-    }
-
     public async Task<ActionResult> OnGetAsync(string author, [FromQuery] int page = 1)
     {
         Cheeps = await service.GetCheepsAsync(page, author);
         return Page();
-    }
-
-    public async Task<IActionResult> OnPostFollowAsync(string UsrnmToFollow) {
-        await lazyGetFollowModel().OnPostFollowAsync(UsrnmToFollow); 
-        return RedirectToPage("");
     }
 }
