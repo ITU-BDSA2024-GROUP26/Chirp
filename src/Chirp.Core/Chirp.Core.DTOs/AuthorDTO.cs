83b00da25f35daff3d20dca6f2631d816c4956a4
Vickihv <89073336+Vickihv@users.noreply.github.com>
2024-11-24 17:23:07 +0100

    Fix 'About me' page

--- src/Chirp.Core/Chirp.Core.DTOs/AuthorDTO.cs
+++ src/Chirp.Core/Chirp.Core.DTOs/AuthorDTO.cs
@@ -10,15 +10,20 @@ public class AuthorDto(string? userName, string? email, ICollection<string>? Fol
 
     public AuthorDto(Author author) : this(author.UserName, author.Email, ExtractFollowersList(author.FollowingList)) { }
 
-    static private ICollection<string> ExtractFollowersList(ICollection<Author> followersList) 
+    static private ICollection<string> ExtractFollowersList(ICollection<Author>? followersList)
     {
-        ICollection<string> res = new string[followersList.Count]; 
-
-        foreach (Author a in followersList) 
+        if (followersList == null)
         {
-            res.Add(a.UserName ?? throw new Exception("Author with null name")); 
+            return new string[0];
         }
 
-        return res; 
+        ICollection<string> res = new string[followersList.Count];
+
+        foreach (Author a in followersList)
+        {
+            res.Add(a.UserName ?? throw new Exception("Author with null name"));
+        }
+
+        return res;
     }
 }
