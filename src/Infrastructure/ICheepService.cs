98f5f5233ac6201f0c73d893ae5aa063061426fd
torbjornjohannsen <toej@itu.dk>
2024-11-27 17:08:12 +0100

    Refactor to add cheeps through CheepService

--- src/Infrastructure/ICheepService.cs
+++ src/Infrastructure/ICheepService.cs
@@ -2,11 +2,11 @@
 
 namespace Infrastructure;
 
-using Core;
 
 public interface ICheepService
 {
     public Task<IEnumerable<CheepDTO>> GetCheepsAsync(int page, string? authorRegex = null);
     public Task<IEnumerable<AuthorDto>> GetFollowingAuthorsAsync(string userName);
     public Task<IEnumerable<CheepDTO>> GetFollowingCheepsAsync(int page, string authorName);
+    public Task SendCheep(string authorName, string content, DateTime timeSent); 
 }
