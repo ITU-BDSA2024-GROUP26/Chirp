3849953c326a0638df0fc9635b2f948dd9807e12
torbjornjohannsen <toej@itu.dk>
2024-12-03 22:59:56 +0100

    modify notification to use the right author key

--- src/Core/Notification.cs
+++ src/Core/Notification.cs
@@ -8,7 +8,7 @@ public class Notification
     [Required]
     public required int cheepID {get; set; } // the cheep the notification is in reference to 
     [Required]
-    public required int authorID { get; set;}
+    public required string authorID { get; set;}
     [Required]
     public required bool tagNotification { get; set; }// are we notifying due to the author being tagged or following 
 
