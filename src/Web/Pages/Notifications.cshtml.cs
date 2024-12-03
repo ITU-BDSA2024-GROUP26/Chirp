f74bb86aa939649c2f5d4017a6c29b09513f4882
torbjornjohannsen <toej@itu.dk>
2024-12-03 21:27:42 +0100

    added pages

---
+++ src/Web/Pages/Notifications.cshtml.cs
@@ -0,0 +1,25 @@
+using Core;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.CodeAnalysis.Elfie.Diagnostics;
+
+namespace Web.Pages;
+
+public class NotificationsModel (ICheepService service, UserManager<Author> userManager) : PageModel
+{
+    public IEnumerable<NotificationDTO> notifications; 
+    public async Task<ActionResult> OnGetAsync([FromQuery] int page = 1) 
+    {
+        if(User.Identity == null || !User.Identity.IsAuthenticated) {
+            
+            return RedirectToPage("/");
+        }
+        var author = await userManager.GetUserAsync(User); 
+        notifications = await service.GetAuthorsNotifications(author!.UserName!); 
+        return Page();
+    }
+    
+}
