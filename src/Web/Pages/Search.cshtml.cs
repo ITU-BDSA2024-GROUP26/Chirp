16ade9867fc3b219ef17fa06b1452a3d8cf8747f
Vickihv <89073336+Vickihv@users.noreply.github.com>
2024-12-04 13:57:34 +0100

    Shows that there is no user named " "

--- src/Web/Pages/Search.cshtml.cs
+++ src/Web/Pages/Search.cshtml.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
 
 
 
@@ -11,28 +13,46 @@ namespace Web.Pages;
 [Authorize]
 public class SearchModel : PageModel
 {
-    UserManager<Author> _userManager;
+    private readonly UserManager<Author> _userManager;
 
     public SearchModel(UserManager<Author> userManager)
     {
         _userManager = userManager;
     }
 
-
+    [BindProperty]
     public string SearchPhrase { get; set; }
-
+    public AuthorDto? CurrentUser { get; set; }
     public IEnumerable<AuthorDto> SearchResults { get; set; } = new List<AuthorDto>();
 
-    public async Task<IActionResult> SearchAsync(string SearchPhrase)
-    {
-        //_userManager.Users.FirstOrDefaultAsync(a => a.UserName == name);
 
-        var users = from u in _userManager.Users
-                    where u.UserName.Contains(SearchPhrase)
-                    select u;
+    public async Task<IActionResult> OnPostSearchAsync(string SearchPhrase)
+    {
+        if (string.IsNullOrWhiteSpace(SearchPhrase))
+        {
+            ModelState.AddModelError(string.Empty, "Search phrase cannot be empty.");
+            return Page();
+        }
+
+        var users = await _userManager.Users
+            .Where(u => u.UserName.Contains(SearchPhrase))
+            .Select(u => new AuthorDto(u.UserName, null, null))
+            .ToListAsync();
+
+        SearchResults = users;
 
         return Page();
     }
+    /*//_userManager.Users.FirstOrDefaultAsync(a => a.UserName == name);
 
+    var users = from u in _userManager.Users
+                where u.UserName.Contains(SearchPhrase)
+                select u;
+
+    users = (IQueryable<Author>)await users.ToListAsync();
+    return Page();
+*/
 }
+
+
 
