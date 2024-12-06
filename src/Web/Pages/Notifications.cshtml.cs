3118228a2037811444118962f71d5f2f077c0b3e
torbjornjohannsen <toej@itu.dk>
2024-12-06 17:04:09 +0100

    rename DTO and Model to follow convention

--- src/Web/Pages/Notifications.cshtml.cs
+++ src/Web/Pages/Notifications.cshtml.cs
@@ -10,15 +10,15 @@ namespace Web.Pages;
 
 public class NotificationsModel (ICheepService service, UserManager<Author> userManager) : PageModel
 {
-    public IEnumerable<NotificationDTO> notifications; 
+    public IEnumerable<NotificationDTO> Notifications; 
     public async Task<ActionResult> OnGetAsync([FromQuery] int page = 1) 
     {
         if(User.Identity == null || !User.Identity.IsAuthenticated) {
             return RedirectToPage("/Public");
         }
         var author = await userManager.GetUserAsync(User); 
-        notifications = await service.GetAuthorsNotifications(author!.UserName!, true); 
-        Console.WriteLine($"Length of notifications in pagemodel {notifications.Count()}");
+        Notifications = await service.GetAuthorsNotifications(author!.UserName!, true); 
+        Console.WriteLine($"Length of notifications in pagemodel {Notifications.Count()}");
         return Page();
     }
 
